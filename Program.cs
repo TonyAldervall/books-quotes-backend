8b04558f4c800abd935532b1e875fd4f022a2efe
Tony Aldervall <tony.aldervall@iths.se>
2025-06-19 16:23:31 +0200

    added automatic migration with error message logging

--- Program.cs
+++ Program.cs
@@ -95,7 +95,16 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
+    try
+    {
+        dbContext.Database.Migrate();
+        Console.WriteLine("Database migrations run successfully!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error while migrating: {ex.Message}");
+        throw;
+    }
 }
 
 if (app.Environment.IsDevelopment())
